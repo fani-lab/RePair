f6f02b254e1c8fc55f5d0d999ed28ed6773bb206
yogeswar Lakshmi Narayanan <yogi2198@gmail.com>
2022-08-26 21:43:02 -0400

    function to add headers to data in tsv files

--- src/dal/msmarco.py
+++ src/dal/msmarco.py
@@ -1,8 +1,9 @@
 import json
 import csv
 import os
-from os.path import exists
 
 from pyserini.search.lucene import LuceneSearcher
 
-searcher = LuceneSearcher.from_prebuilt_index('msmarco-v2-passage')
+searcher = LuceneSearcher.from_prebuilt_index('msmarco-v1-passage')
+
+    
--- src/main.py
+++ src/main.py
@@ -1,8 +1,15 @@
 from os.path import exists
 from common.add_col_name import add_colname
 
-def add_headers():
-    if (exists('../Data/msmarco/qrels.train.tsv')!=True):
-        return add_colname("../Raw/msmarco/qrels.train.tsv",["qid","did","pid","relevancy"])
-check_to_add_headers = add_headers()
 
+raw_data_location  = './Raw/msmarco/'
+clean_data_location = './Data/msmarco/'
+def add_headers(filename,colname):
+
+    if (exists(clean_data_location+filename)!=True):
+        return add_colname(raw_data_location+filename,colname)
+    else:
+        print(f'..fetching queries from {filename}')
+
+headers_for_msmarco_queries = add_headers('qrels.train.tsv',["qid","did","pid","relevancy"])
+headers_for_msmarco_qrels = add_headers('queries.train.tsv',["qid","query"])
