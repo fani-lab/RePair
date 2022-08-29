d6b1981c029fb3e7246a66832a9743e561d59270
yogeswar Lakshmi Narayanan <yogi2198@gmail.com>
2022-08-29 15:05:14 -0400

    converted into mainstream function

--- src/common/add_col_name.py
+++ src/common/add_col_name.py
@@ -1,10 +1,18 @@
 from genericpath import exists
 import pandas as pd
 
-def add_colname(file_location,colname_as_list): 
-    file = pd.read_csv(file_location,sep='\t')   
+def add_colname(filename,colname_as_list,datasetName):
+    '''
+    function inputs a filename,column names and dataset name to add headers. 
+    This is usually required once for adding headers to the data files from the raw folder.
+    since the qrels do no have column names as they exist. 
+    '''
+    store = f'./Raw/{datasetName}/'
+    clean_data_location = f'./Data/{datasetName}/'
+    file_location = clean_data_location+filename
+    file = pd.read_csv(store+filename,sep='\t')   
     file.columns = list(colname_as_list)
-    file.to_csv(file_location, sep="\t", index=False)
+    file_location = file.to_csv(file_location, sep="\t", index=False)
     print(f' printing dataframe after adding headers:\n {file.head()}')
     return file_location
 
--- src/main.py
+++ src/main.py
@@ -1,15 +1,29 @@
+import sys
+import pandas as pd
 from os.path import exists
 from common.add_col_name import add_colname
 
 
-raw_data_location  = './Raw/msmarco/'
-clean_data_location = './Data/msmarco/'
-def add_headers(filename,colname):
+dataset_name = sys.argv[1:] #msmarco, aol,yandex
+raw_data_location  = f'./Raw/{dataset_name[0]}/'
+clean_data_location = f'./Data/{dataset_name[0]}/'
+def add_headers(filename,colname,datasetname):
 
     if (exists(clean_data_location+filename)!=True):
-        return add_colname(raw_data_location+filename,colname)
+        return add_colname(filename, colname, datasetname)
     else:
-        print(f'..fetching queries from {filename}')
+        print(f'..fetching queries from {clean_data_location + filename}')
+        file_store =  pd.read_csv(clean_data_location + filename,sep='\t', chunksize = 100)
+        print(next(file_store))
 
-headers_for_msmarco_queries = add_headers('qrels.train.tsv',["qid","did","pid","relevancy"])
-headers_for_msmarco_qrels = add_headers('queries.train.tsv',["qid","query"])
+# convert this code to a loop later
+add_headers('qrels.train.tsv',["qid","did","pid","relevancy"],dataset_name[0])
+add_headers('queries.train.tsv',["qid","query"],dataset_name[0])
+
+for data in dataset_name:
+    if(data == 'msmarco'):
+        print('processing msmarco...')
+    elif(data == 'aol'):
+        print('processing aol...')
+    else:
+        print('processing yandex...')
