ea90f5af1cb1c1e6b1eae9b79e5d60ecc037cbf8
Yogeswar Lakshmi Narayanan <yogi2198@gmail.com>
2022-08-31 00:38:40 -0400

    fixes to msmarco fetching of passage id

--- src/dal/msmarco.py
+++ src/dal/msmarco.py
@@ -6,6 +6,7 @@ from pyserini.search.lucene import LuceneSearcher
 
 searcher = LuceneSearcher.from_prebuilt_index('msmarco-v1-passage')
 
+
 def fetchDocument(doc_id,searcher):
     try:
         doc = searcher.doc(doc_id)
@@ -13,13 +14,21 @@ def fetchDocument(doc_id,searcher):
         return json_doc['contents']
     except:
         raise ValueError('the provided value is not found in the passage')
-
 def msmarco(qrels_file,queries_file):
-    qrels_source = pd.read_csv(qrels_file, sep='\t', index_col=False)
-    with open(queries_file) as queries:
-        query_source = csv.reader(queries, delimiter='\t')
-        next(query_source)
-        for line in query_source:
-            fetch_qid = qrels_source[qrels_source['qid'] == int(line[0])]
-            get_global_document = int(fetch_qid['pid'])
-            store_document = fetchDocument(get_global_document, searcher)
+    # qrels_source = pd.read_csv(qrels_file, sep='\t', index_col=False)
+    # with open(queries_file) as queries:
+    #     query_source = csv.reader(queries, delimiter='\t')
+    #     next(query_source)
+    #     for line in query_source:
+    #         fetch_qid = qrels_source[qrels_source['qid'] == int(line[0])]
+    #         get_global_document = int(fetch_qid['pid'])
+    #         store_document = fetchDocument(get_global_document, searcher)
+    queries_source = pd.read_csv(queries_file,sep="\t",index_col=False)
+    with open(qrels_file) as qrels:
+        qrels_source = csv.reader(qrels,delimiter='\t')
+        next(qrels_source)
+        for line in qrels_source:
+            fetch_qid = queries_source[queries_source['qid'] == int(line[0])]
+            print(fetch_qid['query'])
+            store_document = fetchDocument((line[2]),searcher)
+            print(store_document)
