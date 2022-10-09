d05bee457c1de2c11a1e07f8d9dc524cf396f423
Hossein Fani <hosseinfani@gmail.com>
2022-10-09 10:47:14 -0400

    Update msmarco.py

--- src/dal/msmarco.py
+++ src/dal/msmarco.py
@@ -24,34 +24,3 @@ def msmarco(input, output):
 
                 qf.write(f"{fetch_qid['qid'].squeeze()}\t{fetch_qid['query'].squeeze()}\n")
                 qp.write(f'{line[2]}\t{retrieved_passage}\n')
-
-def convert_to_features(self, example_batch):
-    # Tokenize contexts and questions (as pairs of inputs)
-
-    if self.print_text:
-        print("Input Text: ", self.clean_text(example_batch['text']))
-    #         input_ = self.clean_text(example_batch['text']) + " </s>"
-    #         target_ = self.clean_text(example_batch['headline']) + " </s>"
-
-    input_ = self.clean_text(example_batch['text'])
-    target_ = self.clean_text(example_batch['headline'])
-
-    source = self.tokenizer.batch_encode_plus([input_], max_length=self.input_length,
-                                              padding='max_length', truncation=True, return_tensors="pt")
-
-    targets = self.tokenizer.batch_encode_plus([target_], max_length=self.output_length,
-                                               padding='max_length', truncation=True, return_tensors="pt")
-
-    return source, targets
-
-
-def __getitem__(self, index):
-    source, targets = convert_to_features(self.dataset[index])
-
-    source_ids = source["input_ids"].squeeze()
-    target_ids = targets["input_ids"].squeeze()
-
-    src_mask = source["attention_mask"].squeeze()
-    target_mask = targets["attention_mask"].squeeze()
-
-    return {"source_ids": source_ids, "source_mask": src_mask, "target_ids": target_ids, "target_mask": target_mask}
