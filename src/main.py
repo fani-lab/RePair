d9da6ee6d6bf17ab4f2e72e204842a0a5e2d8b95
Hossein Fani <hosseinfani@gmail.com>
2022-10-09 11:00:29 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -12,12 +12,12 @@ def run(data_list, domain_list, output, settings):
         datapath = data_list[domain_list.index('msmarco')]
         prep_output = f'./../data/preprocessed/{os.path.split(datapath)[-1]}'
         try:
-            qrels = pd.read_csv(f'{prep_output}/qrels.target.tsv', sep='\t')
-            queries = pd.read_csv(f'{prep_output}/queries.target.tsv', sep='\t')
+            qrels = pd.read_csv(f'{prep_output}/qrels.train.tsv', sep='\t')
+            queries = pd.read_csv(f'{prep_output}/queries.train.tsv', sep='\t')
         except (FileNotFoundError, EOFError) as e:
             msmarco(datapath, prep_output)
-            qrels = pd.read_csv(f'{prep_output}/qrels.target.tsv', sep='\t')
-            queries = pd.read_csv(f'{prep_output}/queries.target.tsv', sep='\t')
+            qrels = pd.read_csv(f'{prep_output}/qrels.train.tsv', sep='\t')
+            queries = pd.read_csv(f'{prep_output}/queries.train.tsv', sep='\t')
         qrels["query"] = queries["query"]
         train(qrels, './../output')
 
