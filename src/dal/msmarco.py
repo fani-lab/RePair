194daaafc5694309beafcc08689f553b0cb97e2a
hosseinfani <hosseinfani@gmail.com>
2022-10-09 12:40:30 -0400

    no message

--- src/dal/msmarco.py
+++ src/dal/msmarco.py
@@ -3,24 +3,27 @@ import pandas as pd
 from ftfy import fix_text
 from pyserini.search.lucene import LuceneSearcher
 
-searcher = LuceneSearcher.from_prebuilt_index('msmarco-v1-passage')
 
 def msmarco(input, output):
-    queries_source = pd.read_csv(f'{input}/queries.target.tsv', sep="\t", index_col=False)
-    with open(f'{input}/qrels.target.tsv') as f:
-        qrels = csv.reader(f, delimiter='\t')
-        next(qrels)
-        with open(f'{output}/qrels.target.tsv', 'w') as pf, open(f'{input}/queries.target.tsv', 'w') as qf:
-            pf.write("pid\tpassage\n")
-            qf.write("qid\tquery\n")
-            for line in pf:
-                fetch_qid = queries_source.loc[queries_source['qid'] == int(line[0])]
-                try:
-                    doc = searcher.doc(line[2])#docid
-                    json_doc = json.loads(doc.raw())
-                    retrieved_passage = fix_text(json_doc['contents'])
-                except:
-                    raise ValueError('the provided docid is not valid')
+    # https://github.com/castorini/pyserini/blob/master/docs/prebuilt-indexes.md
+    searcher = LuceneSearcher.from_prebuilt_index('msmarco-v1-passage')
+    if not searcher:
+        # sometimes you need to manually download the index ==> https://github.com/castorini/pyserini/blob/master/docs/usage-interactive-search.md#how-do-i-manually-download-indexes
+        searcher = LuceneSearcher('./../data/raw/msmarco/lucene-index.msmarco-v1-passage.20220131.9ea315/')
+        if not searcher: raise ValueError("Lucene searcher cannot find/build msmarco index!")
+    queries_source = pd.read_csv(f'{input}/queries.train.tsv', sep="\t", index_col=False, names=['qid', 'query'], header=None)
+    qrels = pd.read_csv(f'{input}/qrels.train.tsv', sep="\t", index_col=False, names=['qid', 'did', 'pid', 'relevancy'], header=None)
+    with open(f'{output}/qrels.train.tsv', 'w') as pf, open(f'{output}/queries.train.tsv', 'w') as qf:
+        pf.write("pid\tpassage\n")
+        qf.write("qid\tquery\n")
+        for row in qrels.itertuples():
+            fetch_qid = queries_source.loc[queries_source['qid'] == int(row.qid)]
+            try:
+                doc = searcher.doc(row.pid)#passage id
+                json_doc = json.loads(doc.raw())
+                retrieved_passage = fix_text(json_doc['contents'])
+            except Exception as e:
+                raise e
 
-                qf.write(f"{fetch_qid['qid'].squeeze()}\t{fetch_qid['query'].squeeze()}\n")
-                qp.write(f'{line[2]}\t{retrieved_passage}\n')
+            qf.write(f"{fetch_qid['qid'].squeeze()}\t{fetch_qid['query'].squeeze()}\n")
+            pf.write(f"{row.pid}\t{retrieved_passage}\n")
