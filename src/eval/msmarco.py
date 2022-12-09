a1469e502451bb7e6cc600497453050d2ec38fa8
Yogeswar Lakshmi Narayanan <yogi2198@gmail.com>
2022-12-08 21:48:26 -0500

    msmarco moved to pyserini search using command line

--- src/eval/msmarco.py
+++ src/eval/msmarco.py
@@ -5,13 +5,12 @@ from tqdm import tqdm
 import pytrec_eval
 import time
 import numpy as np
-import feather
 def getHits(input, output,dataLocation):
     print('retrieving passages using BM25 for alternate queries:\n')
     searcher = LuceneSearcher.from_prebuilt_index('msmarco-v1-passage')
     qrels_file = pd.read_csv(f'./../data/raw/{dataLocation}/qrels.train.tsv', sep='\t', encoding='utf-8', names=['qid', 'did', 'pid', 'relevance'])
     qrels_file = qrels_file.drop(['did', 'relevance'], axis=1)
-    passage_run_dict = {'qid': [], 'pid': [], 'score': []}
+
     if not searcher:
         searcher = LuceneSearcher('./../data/raw/msmarco/lucene-index.msmarco-v1-passage.20220131.9ea315/')
         if not searcher: raise ValueError("Lucene searcher cannot find/build msmarco index!")
@@ -20,36 +19,19 @@ def getHits(input, output,dataLocation):
         predicted_file_list = os.listdir(input)
         for pf in predicted_file_list:
             predicted_queries = pd.read_csv(f'{input}/{pf}', skip_blank_lines=False, sep="/r/r", header=None, engine='python')
-            if not os.path.isfile(f'{output}runs/{dataLocation}/passage_run{pf.split(".")[0][-2:]}.feather'):
+            if not os.path.isfile(f'{output}predictions/{dataLocation}/{pf.split(".")[0]}.tsv'):
+                passage_run_dict = {'qid': [], 'query': []}
                 start = time.time()
                 print(f'getting relevant passages for {predicted_queries.shape[0]} queries for {pf.split(".")[0] if len(predicted_file_list) > 1 else pf}\n')
                 for index,row in tqdm(predicted_queries.itertuples(), total=predicted_queries.shape[0]):
-                    if not isinstance(row, float):
-                        hits = searcher.search(row)
-                        for i in range(len(hits)):
                             passage_run_dict['qid'].append(qrels_file["qid"][index])
-                            passage_run_dict['pid'].append(f'{hits[i].docid:7}')
-                            passage_run_dict['score'].append(f'{hits[i].score:.5f}')
-                    else:
-                        for i in range(10):
-                            passage_run_dict['qid'].append(qrels_file["qid"][index])
-                            passage_run_dict['pid'].append(str(0))
-                            passage_run_dict['score'].append(str(0))
+                            passage_run_dict['query'].append(row)
                 passage_run_df = pd.DataFrame().from_dict(passage_run_dict)
-                passage_run_df.to_feather(f'{output}runs/{dataLocation}/passage_run{pf.split(".")[0][-2:] if len(predicted_file_list) > 1 else ""}.feather')
-                # with open(f'{output}runs/{dataLocation}/passage_run{pf.split(".")[0][-2:] if len(predicted_file_list) > 1 else ""}.tsv', 'w', encoding='utf-8') as retrieved_passage:
-                #     print(f'getting relevant passages for {predicted_queries.shape[0]} queries for {pf.split(".")[0] if len(predicted_file_list) > 1 else pf}\n')
-                #     for index, row in tqdm(predicted_queries.itertuples(), total=predicted_queries.shape[0]):
-                #         if not isinstance(row, float):
-                #             hits = searcher.search(row)
-                #             for i in range(len(hits)):
-                #                 retrieved_passage.write(f'{qrels_file["qid"][index]}\t{hits[i].docid:7}\t{hits[i].score:.5f}\n')
-                #         else:
-                #             for i in range(10):
-                #                 retrieved_passage.write(f'\t{qrels_file["qid"][index]}\t{int(0)}\t{0}\n')
+                passage_run_df['query'] = passage_run_df['query'].astype(str)
+                passage_run_df.to_csv(f'{output}predictions/{dataLocation}/{pf.split(".")[0] if len(predicted_file_list) > 1 else ""}.tsv', sep="\t", index=None, header=None)
                 end = time.time()
                 print(end - start)
-            if not os.path.isfile(f'./../output/metrics/{dataLocation}/passage_run{pf.split(".")[0][-2:] if len(predicted_file_list) > 1 else ""}.feather.metrics'):
+            if not os.path.isfile(f'./../output/metrics/{dataLocation}/passage_run{pf.split(".")[0] if len(predicted_file_list) > 1 else ""}.feather.metrics'):
                 compute_metric(f'./../data/raw/{dataLocation}/qrels.train.tsv', f'{output}runs/{dataLocation}/passage_run{pf.split(".")[0][-2:] if len(predicted_file_list) > 1 else ""}.feather', dataLocation)
             else:
                 print(f'finished computing metrics for ./../output/metrics/{dataLocation}/passage_run{pf.split(".")[0][-2:] if len(predicted_file_list) > 1 else ""}.feather.metrics')
