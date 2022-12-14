597f9d695403a21d68321d9cb1ed05142d5f0c2a
hosseinfani <hosseinfani@gmail.com>
2022-12-14 09:25:54 -0500

    no message

--- mt5.bat
+++ mt5.bat
@@ -1,6 +1,6 @@
-SET PRETRAINED_STEPS=1000000
-SET FINETUNE_STEPS=100
-SET /a STEPS = %PRETRAINED_STEPS% + %FINETUNE_STEPS%
+@SET PRETRAINED_STEPS=1000000
+@SET FINETUNE_STEPS=100
+@SET /a STEPS = %PRETRAINED_STEPS% + %FINETUNE_STEPS%
 
 t5_mesh_transformer ^
 --module_import="numpy" ^
@@ -14,12 +14,3 @@ t5_mesh_transformer ^
 --gin_param="utils.run.init_checkpoint = './output/t5/small/model.ckpt-1000000'" ^
 --gin_param="MIXTURE_NAME = ''" ^
 --gin_param="get_default_spm_path.path = './output/t5/vocabs/cc_all.32000/sentencepiece.model'"
-
-
-@REM --gin_param="t5.data.utils.f = './output/t5/vocabs/cc_all.32000/sentencepiece.model'" 
-@REM --gin_file="./src/mdl/t5/t5/models/gin/dataset.gin" ^	
-@REM --gin_param="SentencePieceVocabulary.sentencepiece_model_file = './output/t5/vocabs/cc_all.32000/sentencepiece.model'"  ^
-@REM --gin_param="tsv_dataset_fn.vocabulary = @SentencePieceVocabulary()" ^ ==> Does not work since seqio does not import gin and this functiontion does not have gin.configurable decorator
-@REM --gin_param="run.vocabulary = @SentencePieceVocabulary()" ==> even if works, at t5.models.mesh_transformer_main.py#181, it is explicitly bind to a function. So, useless gin_param
-@REM The only way is to hard code vocab path 
-@REM --module_import="numpy" ^ just to bypass the default "t5.data.mixtures" at t5.models.mesh_transformer_main#57 that cause the get_default_vocabulary() to be called very soon!
