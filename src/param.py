bccbb466a0fcbd00b4d3d1d88a29bf2e06dc22a8
Yogeswar Lakshmi Narayanan <yogi2198@gmail.com>
2023-01-17 21:28:35 -0500

    fetch trec_eval based on platform

--- src/param.py
+++ src/param.py
@@ -1,11 +1,11 @@
-import random, os, numpy as np, multiprocessing
+import random, os, numpy as np, platform
 import torch
 
 random.seed(0)
 torch.manual_seed(0)
 torch.cuda.manual_seed_all(0)
 np.random.seed(0)
-
+extension = '.exe' if platform.system() == 'Windows' else ""
 os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 
 settings = {
@@ -13,7 +13,7 @@ settings = {
     't5model': 'base.gc',#'base.gc',# 'small.local'
     'ranker': 'bm25',#'qld'
     'metric': 'map',# 'map'
-    'treclib': '"./trec_eval.9.0.4/trec_eval.exe"',#in non-windows, remove .exe, also for pytrec_eval, 'pytrec'
+    'treclib': f'"./trec_eval.9.0.4/trec_eval{extension}"',#in non-windows, remove .exe, also for pytrec_eval, 'pytrec'
     'msmarco.passage': {
         'index': '../data/raw/msmarco.passage/lucene-index.msmarco-v1-passage.20220131.9ea315/',
         'pairing': [None, 'docs', 'query']# [context={msmarco does not have userinfo}, input={query, doc, doc(s)}, output={query, doc, doc(s)}], s means concat of docs
